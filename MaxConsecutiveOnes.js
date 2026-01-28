b56613dbcdaec46e838cc1197fe8cc5b095bfc00
vikash-frontier <vikash@ryuklabs.io>
2026-01-28 22:57:10 +0530

    chore: solved max consecutive ones problem

---
+++ MaxConsecutiveOnes.js
@@ -0,0 +1,36 @@
+{
+  /** 
+    
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+Example 1:
+
+Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+Example 2:
+
+Input: nums = [1,0,1,1,0,1]
+Output: 2
+    */
+}
+
+let nums = [1, 1, 0, 1, 1, 1];
+
+function findMaxConsecutiveOnes(nums) {
+  let count = 0;
+  let maxCount = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1) {
+      count++;
+    } else {
+      maxCount = Math.max(maxCount, count);
+      count = 0;
+    }
+  }
+  return Math.max(maxCount, count);
+}
+
+console.log(findMaxConsecutiveOnes(nums)); // Output: 3
+// Time Complexity: O(n)
+// Space Complexity: O(1)
