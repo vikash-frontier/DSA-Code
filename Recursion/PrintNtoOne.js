097de38990d4391b82c7c61773155bee37f3eea3
vikash-frontier <vikash@ryuklabs.io>
2026-02-02 22:31:29 +0530

    chore: print 1 to n numbes

--- Recursion/PrintNtoOne.js
+++ Recursion/PrintNtoOne.js
@@ -1,9 +1,37 @@
-function printNtoOne(num) {
-  if (num < 1) {
-    return;
-  }
+/**
+ * Print numbers from N to 1 using recursion
+ * @param {number} num - The starting number
+ */
+
+// function printNtoOne(num) {
+//   if (num < 1) {
+//     return;
+//   }
+//   console.log(num);
+//   num = num - 1;
+//   printNtoOne(num);
+// }
+// printNtoOne(5);
+
+/**
+ * Print numbers from 1 to N using recursion
+ * @param {number} num - The starting number
+ */
+// function printOneToN(num, n) {
+//   if (num > n) {
+//     return;
+//   }
+//   console.log(num);
+//   num = num + 1;
+//   printOneToN(num, n);
+// }
+
+let x = 10;
+
+function printOneToN(num) {
+  if (num > x) return;
   console.log(num);
-  num = num - 1;
-  printNtoOne(num);
+  num = num + 1;
+  printOneToN(num);
 }
-printNtoOne(5);
+printOneToN(1);
