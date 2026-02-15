bcab49fb372a27e296542ff50343ad91281f7373
vikash-frontier <vikash@ryuklabs.io>
2026-02-15 14:54:07 +0530

    chore: recursion problrm

--- Recursion/SumOfArray.js
+++ Recursion/SumOfArray.js
@@ -6,11 +6,23 @@
 */
 }
 
-function sumOfArray(arr, n) {
-  if (n === 0) return arr[0];
-  return arr[n] + sumOfArray(arr, n - 1);
+// function sumOfArray(arr, n) {
+//   if (n === 0) return arr[0];
+//   return arr[n] + sumOfArray(arr, n - 1);
+// }
+
+// let arr = [1, 2, 3, 4, 5];
+
+// console.log(sumOfArray(arr, arr.length - 1));
+
+// Find the sum of all the even number in given array using recursion
+
+let arr = [7, 2, 3, 11, 10];
+
+function sumOfOddNumbers(arr, n) {
+  if (n === 0) return arr[0] % 2 === 0 ? arr[0] : 0;
+  let sum = sumOfOddNumbers(arr, n - 1);
+  return arr[n] % 2 === 0 ? sum + arr[n] : sum;
 }
 
-let arr = [1, 2, 3, 4, 5];
-
-console.log(sumOfArray(arr, arr.length - 1));
+console.log(sumOfOddNumbers(arr, arr.length - 1)); // otput: 7 + 3 + 5 = 15
